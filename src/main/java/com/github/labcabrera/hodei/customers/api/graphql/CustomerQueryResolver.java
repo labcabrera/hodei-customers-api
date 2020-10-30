060fde755f1eaa012d017314c1c635a849262215
Luis Cabrera <luis.barragan.gfi@cnppartners.eu>
2020-10-30 19:33:44 +0100

    Version prueba

--- src/main/java/com/github/labcabrera/hodei/customers/api/graphql/CustomerQueryResolver.java
+++ src/main/java/com/github/labcabrera/hodei/customers/api/graphql/CustomerQueryResolver.java
@@ -1,19 +1,38 @@
 package com.github.labcabrera.hodei.customers.api.graphql;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.github.labcabrera.hodei.customers.api.repository.customers.CustomerRepository;
 import com.github.labcabrera.hodei.model.commons.customer.Customer;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class CustomerQueryResolver implements GraphQLQueryResolver {
 
 	@Autowired
 	private CustomerRepository customerRepository;
 
 	public Customer customer(String id) {
+		//TODO meter todo esto a nivel de un servicio
+		if (SecurityContextHolder.getContext().getAuthentication() != null) {
+			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+			List<String> grants = auth.getAuthorities().stream()
+				.map(GrantedAuthority::getAuthority)
+				.filter(e -> !e.startsWith("role-"))
+				.collect(Collectors.toList());
+			log.trace("Grants: {}", grants);
+		}
+		//TODO integrar query con seguridad
 		return customerRepository.findById(id).orElse(null);
 	}
 
--- src/main/java/com/github/labcabrera/hodei/customers/api/repository/customers/CustomerRepository.java
+++ src/main/java/com/github/labcabrera/hodei/customers/api/repository/customers/CustomerRepository.java
@@ -1,12 +1,18 @@
 package com.github.labcabrera.hodei.customers.api.repository.customers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.github.labcabrera.hodei.model.commons.customer.Customer;
 
 public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+	//TODO revisar
+	@Query("{ 'id' : ?0, 'authorization' : { '$in' : ?1} }")
+	Optional<Customer> findById(String id, List<String> auth);
 
 	Optional<Customer> findByIdCardNumber(String idCardNumber);
 
